20c259a11256ca1f8ab4b298bcb042c818d81e9f
Connor (work) <connor.taylor@icloud.com>
2020-09-19 10:56:51 +1200

    update parameters for better fitting

--- removebg.py
+++ removebg.py
@@ -5,11 +5,11 @@ import os
 
 # Hyper params
 BLUR = 21
-CANNY_THRESH_1 = 10
-CANNY_THRESH_2 = 200
+CANNY_THRESH_1 = 20
+CANNY_THRESH_2 = 50
 MASK_DILATE_ITER = 10
 MASK_ERODE_ITER = 10
-MASK_COLOR = (1.0,1.0,1.0) # In BGR format
+MASK_COLOR = (1.0,1.0,1.0)
 
 def main():
     parser = argparse.ArgumentParser(description='backgorund remover')
@@ -22,17 +22,14 @@ def main():
     img = cv2.imread(fn)
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-    #-- Edge detection -------------------------------------------------------------------
+    # Detect edges
     edges = cv2.Canny(gray, CANNY_THRESH_1, CANNY_THRESH_2)
     edges = cv2.dilate(edges, None)
     edges = cv2.erode(edges, None)
 
-    #-- Find contours in edges, sort by area ---------------------------------------------
+    # Find contours
     contour_info = []
     contours, _ = cv2.findContours(edges, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
-    # Previously, for a previous version of cv2, this line was: 
-    #  contours, _ = cv2.findContours(edges, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
-    # Thanks to notes from commenters, I've updated the code but left this note
     for c in contours:
         contour_info.append((
             c,
